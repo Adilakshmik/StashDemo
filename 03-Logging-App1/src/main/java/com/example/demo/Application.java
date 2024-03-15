e4cdb36cabc382182da01584523b28bd3c2897e7
Adilakshmik <adilakshmikoniki6@gmail.com>
2024-03-15 16:18:47 +0530

    m1() added

--- 03-Logging-App1/src/main/java/com/example/demo/Application.java
+++ 03-Logging-App1/src/main/java/com/example/demo/Application.java
@@ -7,9 +7,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
+
 	public void m2(){
 		String a="vani";
 		String b="nani";
+
+	public void m1(){
+		int a=10;
+		int b=20;
+		int c=30;
+
 	}
 
 	public static void main(String[] args) {
