1f3842e87e0b2a18a6e25119e42b1439a272cc65
Adilakshmik <adilakshmikoniki6@gmail.com>
2024-03-15 16:24:46 +0530

    Revert "m2() added"
    This reverts commit 2abe76aafa90c3f8802b75b265015dfcd55b19bd.

--- 03-Logging-App1/src/main/java/com/example/demo/Application.java
+++ 03-Logging-App1/src/main/java/com/example/demo/Application.java
@@ -7,14 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
-	public void m2(){
-		String a="vani";
-		String b="nani";
-	}
-
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
 	}
 
 }
